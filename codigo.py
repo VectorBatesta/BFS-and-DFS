6f262fc57c912b9c855e048801b2c2d1e025d94f
VectorBatesta <vitorbatista@alunos.utfpr.edu.br>
2024-04-24 01:38:07 -0300

    idk

--- codigo.py
+++ codigo.py
@@ -1,7 +1,7 @@
 
 
 #bfs ou dfs
-escolhaProcura = 'bfs'
+escolhaProcura = 'dfs'
 
 
 
@@ -11,24 +11,39 @@ class nodeState:
         self.matriz = matriz
         self.filhos = []
 
-def procuraBFS(raiz):
-    FAZER]
+
+
+def procuraBFS(raiz: nodeState, nodeProcurar: nodeState):
+    fila = [] #fila de nodes
+
+    fila.append(raiz)
+
+    #percorre até acabar
+    while fila != []:
+        nodeAtual = fila.pop(0)
+
+        if nodeAtual == nodeProcurar:
+            return 'achou'
+        
+        fila.append(nodeAtual.filhos)
+
+    return 0 #nao achou
+
+
+
+def procuraDFS(raiz: nodeState, nodeProcurar: nodeState):
+    
     return 'achou'
-    return #nao achou
-
-def procuraDFS(raiz):
-    FAZER]
-    return 'achou'
-    return #nao achou
+    return 0 #nao achou
 
 
 
-def gerar_filhos(node: nodeState):
+def gerar_filhos(nodePai: nodeState, raiz: nodeState):
     adicionarFilhos = [] #checa depois se há repetido
 
     #acha a posicao do zero pra fazer trocas
     for i in range(9):
-        if node.matriz[i] == 0:
+        if nodePai.matriz[i] == 0:
             posicao = i
             break
         else:
@@ -39,7 +54,7 @@ def gerar_filhos(node: nodeState):
     #[XX ]
     #troca direita
     if posicao in (0, 1, 3, 4, 6, 7):
-        novoFilho = nodeState(node.matriz)
+        novoFilho = nodePai
         novoFilho.matriz[posicao] = novoFilho.matriz[posicao + 1]
         novoFilho.matriz[posicao + 1] = 0
         adicionarFilhos.append(novoFilho)
@@ -49,7 +64,7 @@ def gerar_filhos(node: nodeState):
     #[ XX]
     #troca esquerda
     if posicao in (1, 2, 4, 5, 7, 8):
-        novoFilho = nodeState(node.matriz)
+        novoFilho = nodePai
         novoFilho.matriz[posicao] = novoFilho.matriz[posicao - 1]
         novoFilho.matriz[posicao - 1] = 0
         adicionarFilhos.append(novoFilho)
@@ -59,7 +74,7 @@ def gerar_filhos(node: nodeState):
     #[   ]
     #troca baixo
     if posicao in (0, 1, 2, 3, 4, 5):
-        novoFilho = nodeState(node.matriz)
+        novoFilho = nodePai
         novoFilho.matriz[posicao] = novoFilho.matriz[posicao + 3]
         novoFilho.matriz[posicao + 3] = 0
         adicionarFilhos.append(novoFilho)
@@ -69,7 +84,7 @@ def gerar_filhos(node: nodeState):
     #[XXX]
     #troca cima
     if posicao in (3, 4, 5, 6, 7, 8):
-        novoFilho = nodeState(node.matriz)
+        novoFilho = nodePai
         novoFilho.matriz[posicao] = novoFilho.matriz[posicao - 3]
         novoFilho.matriz[posicao - 3] = 0
         adicionarFilhos.append(novoFilho)
@@ -78,7 +93,13 @@ def gerar_filhos(node: nodeState):
     while adicionarFilhos != []:
         proxFilho = adicionarFilhos.pop()
         match escolhaProcura:
-            case 'bfs'
+            case 'bfs':
+                achou = procuraBFS(raiz, proxFilho)
+            case 'dfs':
+                achou = procuraDFS(raiz, proxFilho)
+        
+        if achou == 0:
+            nodePai.filhos.append(proxFilho)
         
 
 
@@ -95,20 +116,25 @@ def gerar_filhos(node: nodeState):
 def algoritmoDFS(raiz):
     pilha = []
 
-    while procuraDFS(raiz) != 'achou':
+    while procuraDFS(raiz) == 0:
+        return
         
 
 
 
 
 def algoritmoBFS(raiz):
+    fila = []
+
+    while procuraBFS(raiz) == 0:
+        return
     
 
 if __name__ == "__main__":
     #0 = vazio
     raiz = nodeState([1, 2, 4, 3, 0, 5, 6, 7, 8])
     objetivo =       [1, 2, 3, 4, 5, 6, 7, 8, 0]
-    
+
     match escolhaProcura:
         case 'bfs':
             algoritmoBFS(raiz)
